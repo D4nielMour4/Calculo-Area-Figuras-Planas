b983c5f29e8956f89992b5415dfc79db9b922482
D4nielMour4 <moura.s.c.danieL@gmail.com>
2022-12-15 17:50:49 -0300

    Att2.0

--- Area de Figuras/src/Aplicacao/Retangulo.java
+++
@@ -1,9 +0,0 @@
-package Aplicacao;
-
-
-public class Retangulo extends Modelo{
-    @Override
-    public double Tipofigura(){
-        return getBase() * getAltura();
-    }
-}
--- Area de Figuras/src/Aplicacao/Triangulo.java
+++
@@ -1,9 +0,0 @@
-package Aplicacao;
-
-
-public class Triangulo extends Modelo{
-    @Override
-    public double Tipofigura(){
-        return (getBase()*getAltura()) / 2;
-    }
-}
